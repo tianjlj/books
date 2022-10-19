05b4c16c89bd2e06599476e6e8f38a4c26c8a331
Aaron.tian <415913657@qq.com>
2022-10-19 14:27:54 +0800

    test

---
+++ server/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+if __name__ == '__main__':
+    ...
